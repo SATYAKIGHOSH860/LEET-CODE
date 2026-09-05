779b0f4b45759ce9c65394a124731670404eb809
SATYAKI GHOSH <ghoshsatyaki860@gmail.com>
2026-09-06 02:07:24 +0530

    Time: 10 ms (36.1%), Space: 19.7 MB (53.75%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean = ''
+        for ch in s:
+            if ch.isalnum():
+                clean = clean + ch.lower()
+
+        if clean == clean[::-1]:
+            return True
+        else:
+            return False
